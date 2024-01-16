2c646ef3fcf76e79ac2b8f3a286113e54429d883
DiottoDev <nicodiottodev@gmail.com>
2024-01-16 00:30:02 -0300

    change :hammer: (server): add start function server

--- src/server.ts
+++ src/server.ts
@@ -13,41 +13,46 @@ import {
 } from "@/routers";
 import cors from "@elysiajs/cors";
 
-const server = new Elysia()
-	.onError(({ error, set }) => {
-		return {
-			status: set.status,
-			message: error.message,
-			data: null,
-		};
-	})
-	.use(
-		cors({
-			credentials: true,
-			allowedHeaders: ["Content-type"],
-			methods: ["GET", "DELETE", "PATCH", "POST", "PUT", "HEAD", "OPTIONS"],
-			origin: (request): boolean => {
-				const origin = request.headers.get("origin");
-				if (!origin) {
-					return false;
-				}
-				return true;
-			},
-		}),
-	)
-	.get("/", () => "🦊 Server is running")
-	.use(create_user_router)
-	.use(get_user_router)
-	.use(update_user_router)
-	.use(send_email_auth_router)
-	.use(create_todo_router)
-	.use(get_todo_router)
-	.use(get_todos_router)
-	.use(get_todos_from_status_router)
-	.use(update_todo_router)
-	.use(delete_todo_router)
-	.listen(3000);
+const server = () =>
+	new Elysia()
+		.get("/", () => "🦊 Server is running")
+		.onError(({ error, set }) => {
+			return {
+				status: set.status,
+				message: error.message,
+				data: error,
+			};
+		})
+		.use(
+			cors({
+				credentials: true,
+				allowedHeaders: ["Content-type"],
+				methods: ["GET", "DELETE", "PATCH", "POST", "PUT", "HEAD", "OPTIONS"],
+				origin: (request): boolean => {
+					const origin = request.headers.get("origin");
+					if (!origin) {
+						return false;
+					}
+					return true;
+				},
+			}),
+		)
+		.use(create_user_router)
+		.use(get_user_router)
+		.use(update_user_router)
+		.use(send_email_auth_router)
+		.use(create_todo_router)
+		.use(get_todo_router)
+		.use(get_todos_router)
+		.use(get_todos_from_status_router)
+		.use(update_todo_router)
+		.use(delete_todo_router)
+		.listen(3000);
 
-console.log("🦊 Server is running at htpp://localhost:3000");
+console.log("🦊 Server is running at http://localhost:3000");
 
 export type APP = typeof server;
+
+(() => {
+	server();
+})();
