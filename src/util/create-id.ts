707e453e4741435210bf9850bc30414e1b92d8e9
DiottoDev <nicodiottodev@gmail.com>
2024-01-18 10:50:45 -0300

    change :hammer: (utils): update create id function

--- src/util/create-id.ts
+++ src/util/create-id.ts
@@ -1,7 +1,7 @@
 export const create_id = () => {
 	const char =
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
-	const length = 16;
+	const length = 32;
 
 	return Array.from({ length }, () =>
 		char.charAt(Math.floor(Math.random() * char.length)),
